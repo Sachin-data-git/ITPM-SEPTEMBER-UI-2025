2f13536dc0e5b5b37a35b8cc4e3d1e1b77cb6051
sachin <sachin.g@itpreneurpune.com>
2026-02-27 10:57:39 +0530

    JS UPDATED

---
+++ JavaScript/ImportExport/script1.js
@@ -0,0 +1,12 @@
+let fname="Aman";
+let lname="Shinde"
+
+function demo(){
+    return("demo")
+}
+
+//single values
+// export default fname;
+
+
+export  {fname,lname,demo};
---
+++ JavaScript/ImportExport/script2.js
@@ -0,0 +1,4 @@
+import {fname,lname,demo} from "./script1.js"
+
+console.log("Hello My Name is "+fname +" "+lname)
+console.log(demo())
